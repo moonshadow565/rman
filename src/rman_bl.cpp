76233e7452dc064360f878a09603a42313d950a3
moonshadow565 <moonshadow565@hotmail.com>
2025-08-15 00:03:36 +0200

    rman-bl multi-manifest support

--- src/rman_bl.cpp
+++ src/rman_bl.cpp
@@ -11,30 +11,30 @@ using namespace rlib;
 
 struct Main {
     struct CLI {
-        std::string manifest = {};
+        std::vector<std::string> inputs = {};
         std::string format = {};
     } cli = {};
 
     auto parse_args(int argc, char** argv) -> void {
         argparse::ArgumentParser program(fs::path(argv[0]).filename().generic_string());
         program.add_description("Lists bundle names used in manifest.");
-        program.add_argument("manifest").help("Manifest file to read from.").required();
         program.add_argument("--format").help("Format output.").default_value(std::string("/{bundleId}.bundle"));
-
+        program.add_argument("input").help("Manifest file(s) or folder(s) to read from.").remaining().required();
         program.parse_args(argc, argv);
-
-        cli.manifest = program.get<std::string>("manifest");
         cli.format = program.get<std::string>("format");
+        cli.inputs = program.get<std::vector<std::string>>("input");
     }
 
     auto run() -> void {
-        rlib_trace("Manifest file: %s", cli.manifest.c_str());
-        auto manifest = RMAN::read_file(cli.manifest);
-
-        for (auto const& bundle : manifest.bundles) {
-            fmt::dynamic_format_arg_store<fmt::format_context> store{};
-            store.push_back(fmt::arg("bundleId", bundle.bundleId));
-            std::cout << fmt::vformat(cli.format, store) << std::endl;
+        auto paths = rlib::collect_files(cli.inputs, [](fs::path const& p) { return p.extension() == ".manifest"; });
+        for (auto const& path : paths) {
+            rlib_trace("Manifest file: %s", path.generic_string().c_str());
+            auto manifest = RMAN::read_file(path);
+            for (auto const& bundle : manifest.bundles) {
+                fmt::dynamic_format_arg_store<fmt::format_context> store{};
+                store.push_back(fmt::arg("bundleId", bundle.bundleId));
+                std::cout << fmt::vformat(cli.format, store) << std::endl;
+            }
         }
     }
 };
